02ceda77fcaa3e1bc8492dbca578a33df75f2573
gearhead2k <gearhead25k@gmail.com>
2015-01-07 18:59:29 -0500

    new checkin

---
+++ src/MemoryGame/MemoryGame/App/app.js
@@ -0,0 +1,16 @@
+﻿    'use strict';
+
+    var memoryApp = angular.module('memoryGameApp', ['ngRoute', 'memoryGameModule']);
+
+    memoryApp.config([
+        '$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/game',
+            {
+                templateUrl: '/app/MemoryGame/MemoryGameBoard.htm',
+                controller: 'memoryGameController'
+            }).otherwise({
+                templateUrl: '/app/MemoryGame/MemoryGameBoard.htm',
+                controller: 'memoryGameController'
+            });
+        }
+    ]);
